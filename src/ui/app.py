49ec2ab06031b5bb598a1853cb4db4c3775a560c
BohdanPenkov <bohdanpenkov@planeks.net>
2025-01-02 15:46:35 +0200

    well fixes

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import sys
-import os
-
-
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-sys.path.append(project_root)
 
 
 def main():
@@ -14,16 +8,16 @@ def main():
     """
 
     document_library_page = st.Page(
-        "pages/document_liabrary.py",
+        "ui_pages/document_liabrary.py",
         title="Document Library",
         default=True,
     )
     document_detail_page = st.Page(
-        "pages/document_detail.py",
+        "ui_pages/document_detail.py",
         title="Document Detail",
     )
     chat_page = st.Page(
-        "pages/chat.py",
+        "ui_pages/chat.py",
         title="Chat",
     )
 
@@ -34,5 +28,4 @@ def main():
 
 
 if __name__ == "__main__":
-    print(project_root)
     main()
